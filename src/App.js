585613faf417c5593a8408966c807c743f40bee6
Ruhani FashionFantasy <ruhanifashionfantasy@Ruhanis-MacBook-Air.local>
2023-09-19 16:05:33 +0530

    Project done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import "./App.css"
 import Header from './components/Header/Header';
-import Filters from './components/Filters/Filters';
-import Card from './components/Card/Card';
+
 
 const App = () => {
   return (
